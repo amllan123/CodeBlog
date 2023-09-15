3f1d9faaaf3b4019e324559d7be02c8b697666ba
amllan123 <99805797+amllan123@users.noreply.github.com>
2023-09-15 12:44:25 +0530

    Feature Component API update

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,11 +2,27 @@ import React from "react";
 import styles from "./featured.module.css";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
+import Link from "next/link";
+
+const url=process.env.NEXTAUTH_URL
+
+const getData = async (page,cat) => {
+  const res = await fetch(`${url}/api/posts?page=${page}&cat=${cat || ""}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
 
 
+const Featured = async () => {
 
-const Featured = () => {
-  const logo = "<span className={styles.titleLogo}>CodeBlog</span>";
+  const { posts, count } = await getData(1," ");
+  
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
@@ -24,12 +40,13 @@ const Featured = () => {
           />
         </div>
         <div className={styles.textContainer}>
-          <h2 className={styles.postTitle}>Next.js vs React.js</h2>
-          <p className={styles.postDesc}>
-            In the world of web development, React.js has been a dominant player
-            ...
+          <h2 className={styles.postTitle}>{posts[0].title}</h2>
+          <p className={styles.postDesc} dangerouslySetInnerHTML={{__html:posts[0]?.desc.substring(0, 100)+'...'}}>
+          
           </p>
-          <button className={styles.postButton}>Read More</button>
+          <Link href={`/posts/${posts[0].id}`} className={styles.link}>
+          Read More
+        </Link>
         </div>
       </div>
     </div>
